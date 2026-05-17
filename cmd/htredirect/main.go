6326b9ddb20edbf933640785922d355c82d39918
lugosieben <62535192+lugosieben@users.noreply.github.com>
2026-05-17 21:49:35 +0200

    hotfix

--- cmd/htredirect/main.go
+++ cmd/htredirect/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/lugosieben/htredirect/config"
 	"github.com/lugosieben/htredirect/internal/redirectserver"
@@ -11,6 +14,12 @@ import (
 func main() {
 	fmt.Printf("htredirect %s\n", config.VERSION)
 	config.Load()
-	webserver.Start(config.WebPort)
-	redirectserver.Start(config.Port)
+
+	go webserver.Start(config.WebPort)
+	go redirectserver.Start(config.Port)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+	fmt.Println("exiting")
 }
